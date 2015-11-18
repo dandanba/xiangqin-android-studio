ae800f04378663d3fa5b683042b78fb4740fceb0
dandanba <13641194007@139.com>
2015-11-18 10:43:24 +0800

    project -> arr

--- xiangqin/app/src/main/java/com/xiangqin/app/activity/IntentGenerator.java
+++ xiangqin/app/src/main/java/com/xiangqin/app/activity/IntentGenerator.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
+import cn.smssdk.gui.layout.TitleLayout;
+
 /**
  * Created by dandanba on 11/13/15.
  */
@@ -12,5 +14,4 @@ public class IntentGenerator {
     public static Intent genSimpleActivityIntent(Context context, Class<?> clazz) {
         return new Intent(context, clazz);
     }
-
 }
